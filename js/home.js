7bbea65ec4c7b86781a4b304815f07047e8febc8
Bryan Hinahon <bryanhinahon99@gmail.com>
2026-07-20 16:12:17 +0800

    add animation for paragraph

--- js/home.js
+++ js/home.js
@@ -1,26 +1,34 @@
 const profile = document.querySelector('.avatar');
-const  homeText = document.querySelector('.text');
+const homeText = document.querySelector('.text');
 
 
 const menuBtn = document.querySelector('.menu-btn');
 const nav = document.querySelector('.navigator');
 
 
-menuBtn.addEventListener('click', ()=>{
+
+const paragraph = document.querySelectorAll('.AboutMe');
+
+
+menuBtn.addEventListener('click', () => {
   nav.classList.toggle('active');
 });
-const observer = new IntersectionObserver((entries)=>{
+const observer = new IntersectionObserver((entries) => {
 
-    entries.forEach((entry)=>{
-       if(entry.isIntersecting){
-        setTimeout(()=>{
-          entry.target.classList.add('show');
-        }, 100);
-       }
-    })
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('show');
+      }, 100);
+    }
+  })
 });
 
 const elements = [profile, homeText];
-elements.forEach((elem)=>{
-    observer.observe(elem);
+elements.forEach((elem) => {
+  observer.observe(elem);
+})
+
+paragraph.forEach((about) => {
+  observer.observe(about);
 })
