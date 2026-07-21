d81fc3cfb71eb3a7532afef25395ab12112fc315
Bryan Hinahon <bryanhinahon99@gmail.com>
2026-07-21 21:05:05 +0800

    add animation for about cards

--- js/home.js
+++ js/home.js
@@ -5,8 +5,7 @@ const homeText = document.querySelector('.text');
 const menuBtn = document.querySelector('.menu-btn');
 const nav = document.querySelector('.navigator');
 
-
-
+const infoCards = document.querySelectorAll('.info-card');
 const paragraph = document.querySelectorAll('.AboutMe');
 
 
@@ -15,11 +14,14 @@ menuBtn.addEventListener('click', () => {
 });
 const observer = new IntersectionObserver((entries) => {
 
+
   entries.forEach((entry) => {
+
+    const delay = [...infoCards].indexOf(entry.target) * 180; //it jobs is for delay animation for cards
     if (entry.isIntersecting) {
       setTimeout(() => {
         entry.target.classList.add('show');
-      }, 100);
+      }, delay);
     }
   })
 });
@@ -27,6 +29,10 @@ const observer = new IntersectionObserver((entries) => {
 const elements = [profile, homeText];
 elements.forEach((elem) => {
   observer.observe(elem);
+})
+
+infoCards.forEach((cards)=>{
+  observer.observe(cards);
 })
 
 paragraph.forEach((about) => {
